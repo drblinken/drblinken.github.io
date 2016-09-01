f746f6bff310feab23363bcbdb438c157eae349f
Dr Blinken <drblinken@gmail.com>
2016-09-01 09:59:44 +0200

    little update

--- aliasses.sh
+++ aliasses.sh
@@ -1,5 +1,5 @@
 alias ow='open http://localhost:4000/bentos/7web/week2.html'
 alias os='open http://localhost:4000'
 alias gh='open http://drblinken.github.io'
-alias rj='jekyll serve -w'
+alias rj='jekyll serve --drafts'
 cat aliasses.sh
